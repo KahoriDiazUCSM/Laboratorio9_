f15f14da6ed8b09df1bb3466321f473734adefb2
KahoriDiazUCSM <131702350+KahoriDiazUCSM@users.noreply.github.com>
2024-11-23 22:59:12 -0500

    Create Prueba.java

---
+++ ACTIVIDAD1/Prueba.java
@@ -0,0 +1,12 @@
+package actividad;
+import javax.swing.*;
+
+
+public class Prueba {
+    public static void main(String[] args) {
+        VentanaInicio ventanaInicio = new VentanaInicio();
+        ventanaInicio.setVisible(true);
+       
+    }
+    
+}
